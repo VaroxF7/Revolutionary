7279a239bbc1fdb54326c31b75203865af524c78
VaroxF7 <149310259+VaroxF7@users.noreply.github.com>
2025-01-02 13:32:46 +0100

    Delete kubejs directory

--- kubejs/server_scripts/tags.js
+++
@@ -1,8 +0,0 @@
-ServerEvents.tags('fluid', event => {
-    event.add("forge:steel", "createbigcannons:molten_steel")
-})
-
-ServerEvents.tags('worldgen/biome', event => {
-    event.removeAll('ad_astra:has_structure/meteor_biomes')
-    event.removeAll('ad_astra:has_structure/oil_well_biomes')
-})
